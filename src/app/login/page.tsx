a1b70d7dc17220e352de1e5909295d189d8afb5d
JeetDas5 <jeet15083011@gmail.com>
2026-05-25 01:15:33 +0530

    fix: store auth data

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,42 +1,61 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 import { loginAdmin } from "@/lib/services/auth.service";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
+  const router = useRouter();
 
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = async () => {
     try {
+      setLoading(true);
+
       await loginAdmin(email, password);
-      alert("Logged in");
+
+      router.push("/dashboard");
     } catch (error) {
       console.error(error);
+      alert("Invalid credentials");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div className="p-10 flex flex-col gap-4 max-w-sm">
-      <input
-        placeholder="Email"
-        className="border p-2"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-md mx-auto flex flex-col gap-4 border p-6 rounded-xl">
+        <h1 className="text-2xl font-bold">StockTrack Login</h1>
 
-      <input
-        placeholder="Password"
-        type="password"
-        className="border p-2"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-2 rounded-md"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-      <button onClick={handleLogin} className="bg-black text-white p-2">
-        Login
-      </button>
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-2 rounded-md"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          onClick={handleLogin}
+          disabled={loading}
+          className="bg-black text-white p-2 rounded-md"
+        >
+          {loading ? "Loading..." : "Login"}
+        </button>
+      </div>
     </div>
   );
 }
